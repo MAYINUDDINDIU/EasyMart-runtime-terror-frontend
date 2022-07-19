1199b5ee2ae3968429daab598d805edeceb9fb9e
MAYINUDDIN <mainu768971@gmail.com>
2022-07-19 11:45:26 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#22c55e",
+          "secondary": "#3b82f6",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
