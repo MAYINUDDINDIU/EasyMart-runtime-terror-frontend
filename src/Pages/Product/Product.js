8288737613ecf725dd18e0f985a49045e4bc3213
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-07-21 00:22:42 +0600

    top margin increased in carousel slider and Card heading background color changed

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -14,14 +14,14 @@ const Product = () => {
 
     return (
         <div className='justify-center mt-20'>
-            <h2 className='text-center lg:text-2xl drop-shadow mt-4 mb-1 font-bold'>OUR BEST PRODUCTS</h2>
+            <h2 className='text-center lg:text-4xl drop-shadow mt-4 mb-10 font-bold'>OUR BEST PRODUCTS</h2>
             <div className='grid px-6 lg:px-16 grid-col-4 lg:grid-cols-5 gap-4  mt-5 mb-5 flex justify-center'>
                 <div class="card bordered rounded lg:w-60 w-72 h-80 bg-base-100 hover:scale-105 duration-500 shadow-xl">
                     <figure><img className='h-48 lg:w-60 w-72' src={p1} alt="Shoes" /></figure>
                     <h6 class=" font-bold text-xl text-dark mt-3">
                         Geans Pant for man
                         </h6>
-                    <p className='text-center'> <span className='text-secondary  text-lg font-bold'>105$</span></p>
+                    <p className='text-center'> <span className='text-success  text-lg font-bold'>105$</span></p>
                     <div class="card-actions justify-center">
                         <button className='btn btn-secondary btn-sm  rounded px-12 mt-3 ' >BUY NOW</button>
                     </div>
