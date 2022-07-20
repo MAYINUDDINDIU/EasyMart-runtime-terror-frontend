6cf51ea0c96fc8b56b8491cc7c8ae90e926f83ac
MAYINUDDIN <mainu768971@gmail.com>
2022-07-20 08:43:35 -0700

    product update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carousel from '../../utilities/Carousel/Carousel';
+import Product from '../Product/Product';
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
+            <Product></Product>
         </div>
     );
 };
