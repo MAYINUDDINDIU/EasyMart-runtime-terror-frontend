b284c8a83153c7b484d0c5f19de491b4c40a0ac5
saba rahman <sabarahman.official@gmail.com>
2022-07-20 21:21:35 +0600

    footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carousel from '../../utilities/Carousel/Carousel';
+import Footer from '../Shared/Footer';
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
+            <Footer />
         </div>
     );
 };
