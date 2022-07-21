cf2f2a20eb87f416caa5ab9999f51438fc44f9a9
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-07-21 15:11:56 +0600

    searchbar shifted to right

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -6,33 +6,7 @@ import { BsFillBagCheckFill } from "react-icons/bs";
 const Navbar = () => {
   const navItems = (
     <>
-      <li>
-        <div class="form-control">
-          <div class="input-group">
-            <input
-              type="text"
-              placeholder="Search…"
-              class="input input-bordered text-black bg-white"
-            />
-            <button class="btn btn-square bg-success border-0 text-black hover:bg-success">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </li>
+     
 
       <li>
         <Link to="/" className="font-bold">
@@ -89,6 +63,31 @@ const Navbar = () => {
         <ul class="menu menu-horizontal p-0 text-white ">{navItems}</ul>
       </div>
       <div class="navbar-end">
+      <div class="form-control mr-10">
+          <div class="input-group">
+            <input
+              type="text"
+              placeholder="Search…"
+              class="input input-bordered text-black bg-white"
+            />
+            <button class="btn btn-square bg-success border-0 text-black hover:bg-success">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
         <Link to="/login" className="text-white font-bold mr-3">
           Login
         </Link>
