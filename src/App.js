152b4b31ac5569654d94d8b57dc16ed130e5893a
Sadia Islam <sadia.islam7210@gmail.com>
2022-07-21 14:07:52 +0600

    Contact Us added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Navbar from './Pages/Shared/Navbar';
 import Home from './Pages/Home/Home';
 import CarouselSlider from './componets/CarouselSlider/CarouselSlider';
 import Footer from './Pages/Shared/Footer';
+import Contact from './component/ContactUs/Contact';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 
 function App () {
@@ -13,11 +18,12 @@ function App () {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>} >Home</Route>
+        <Route path='/contact' element={<Contact/>} ></Route>
 
       </Routes>
-      <CarouselSlider></CarouselSlider>
+      {/* <CarouselSlider></CarouselSlider> */}
       <Footer></Footer>
-
+      <ToastContainer />
     </div>
   );
 }
