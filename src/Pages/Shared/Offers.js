7192377ce97bb11ec4e3165aa5772236a89609fc
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-08-04 00:17:05 +0600

    icons added to the offer section

--- src/Pages/Shared/Offers.js
+++ src/Pages/Shared/Offers.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { RiCoupon2Fill } from "react-icons/ri";
+import { TbDiscount2 } from "react-icons/tb";
+import { AiOutlineGift } from "react-icons/ai";
 const Offers = () => {
   return (
     <div>
@@ -8,13 +10,18 @@ const Offers = () => {
           Offers
         </h3>
         <div className="flex">
-          <p>Coupons</p>{" "}
+          <p className="flex items-center">
+            <RiCoupon2Fill className="mr-2"></RiCoupon2Fill> Coupons
+          </p>{" "}
           <p className="mx-3 h-7 w-7 p-1 border border-red-500 rounded-full text-sm text-red-500 text-center font-bold">
             1
           </p>
         </div>
-        <p>Gift</p>
-        <p>
+        <p className="flex items-center">
+          <AiOutlineGift className="mr-2"></AiOutlineGift>Gift
+        </p>
+        <p className="flex items-center">
+          <TbDiscount2 className="mr-2"></TbDiscount2>
           Discounts{" "}
           <span className="mx-3 h-7 w-7 p-1 border border-red-500 rounded-full text-sm text-red-500 text-center font-bold">
             86
