9f4308ba7d1915b5430c3debf1223524f791722c
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-08-05 00:17:07 +0600

    added hovering effect on card images in products section

--- src/Pages/Product/SingleProduct.js
+++ src/Pages/Product/SingleProduct.js
@@ -1,28 +1,34 @@
-import React from 'react';
-
+import React from "react";
+import "./SingleProduct.css";
 const SingleProduct = ({ pd }) => {
-    const { name, img, price, quantity } = pd;
-    return (
-        <div className="card bordered rounded lg:w-60 w-72 h-80 hover:scale-105 duration-500 cardBg shadow-xl">
-            <figure>
-                <img className="h-48 lg:w-60 w-72" src={img} alt="Shoes" />
-            </figure>
-            <h6 className=" font-bold text-xl text-dark mt-3">{name}</h6>
-            <p className="text-center">
-                {" "}
-                <span className="text-neutral  text-lg font-bold">{price}$</span>
-            </p>
-            <p className="text-center">
-                {" "}
-                <span className="text-neutral  text-lg font-bold">Available: {quantity}</span>
-            </p>
-            <div className="card-actions justify-center">
-                <button className="btn btn-secondary btn-sm  rounded px-12 mt-3 ">
-                    BUY NOW
-                </button>
-            </div>
+  const { name, img, price, quantity } = pd;
+
+  return (
+    <div className="card bordered rounded lg:w-60 w-72 h-80 hover:scale-105 duration-500 cardBg shadow-xl cursor-pointer">
+      <figure className="card_image">
+        <img className="h-48 lg:w-60 w-72 " src={img} alt="Shoes" />
+      </figure>
+      <div className="p-3">
+        {" "}
+        <h1 className=" font-bold text-2xl text-dark mt-1">{name}</h1>
+        <p className="text-center">
+          {" "}
+          <span className="text-neutral  text-lg font-bold">{price}$</span>
+        </p>
+        <p className="text-center">
+          {" "}
+          <span className="text-neutral  text-lg font-bold">
+            Available: {quantity}
+          </span>
+        </p>
+        <div className="card-actions justify-center">
+          <button className="btn btn-secondary btn-sm  rounded px-12 mt-3 ">
+            BUY NOW
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
