e2a4d0c416ae826c78c6b5490c5ee6721b6b0dff
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-08-05 01:40:46 +0600

    added details button which will appear on hovering at the product image

--- src/Pages/Product/SingleProduct.js
+++ src/Pages/Product/SingleProduct.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SingleProduct.css";
 const SingleProduct = ({ pd }) => {
   const { name, img, price, quantity } = pd;
-
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
   return (
     <div className="card bordered rounded lg:w-60 w-72 h-80 hover:scale-105 duration-500 cardBg shadow-xl cursor-pointer">
-      <figure className="card_image">
-        <img className="h-48 lg:w-60 w-72 " src={img} alt="Shoes" />
-      </figure>
+      <section
+        className="relative"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        <figure className="card_image">
+          <div>
+            <img className="h-48 lg:w-60 w-72 " src={img} alt="Shoes" />
+          </div>
+        </figure>
+        {isHovering && (
+          <button className="btn  bg-secondary hover:bg-secondary text-black border-0  absolute bottom-0 left-0 w-full rounded-none">
+            Details &nbsp;
+            <ion-icon name="chevron-forward-outline"></ion-icon>
+          </button>
+        )}
+      </section>
+
       <div className="p-3">
         {" "}
         <h1 className=" font-bold text-2xl text-dark mt-1">{name}</h1>
