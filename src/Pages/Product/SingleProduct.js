6e75316f9cfa82e53bd92f09e06ac1abbc976e32
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-08-10 01:13:19 +0600

    edit

--- src/Pages/Product/SingleProduct.js
+++ src/Pages/Product/SingleProduct.js
@@ -18,7 +18,7 @@ const SingleProduct = ({ pd }) => {
             </p>
             <div className="card-actions justify-center">
                 <button className="btn btn-secondary btn-sm  rounded px-12 mt-3 ">
-                    BUY NOWd
+                    BUY NOW
                 </button>
             </div>
         </div>
