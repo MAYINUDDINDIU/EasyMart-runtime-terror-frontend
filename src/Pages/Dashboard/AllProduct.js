78654f4eb1fd22df5f01a9f468257ca9972719c3
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-08-14 14:21:23 +0600

    dashboard updated and products are displayed in table, also summery component used for showing total orders, total sales and total products

--- src/Pages/Dashboard/AllProduct.js
+++ src/Pages/Dashboard/AllProduct.js
@@ -1,39 +1,43 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { deleteProduct } from '../../features/productSlice';
-
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { deleteProduct } from "../../features/productSlice";
+import { MdModeEditOutline, MdDelete } from "react-icons/md";
 const AllProduct = ({ pd }) => {
-    const { _id, name, img, price, quantity } = pd;
-    const dispatch = useDispatch();
+  const { _id, name, img, price, quantity } = pd;
+  const dispatch = useDispatch();
 
-    const handleDelete = id => {
-        dispatch(deleteProduct(id))
-    }
-    return (
-        <div className="card bordered rounded lg:w-60 w-72 hover:scale-105 duration-500 cardBg shadow-xl">
-            <figure>
-                <img className="h-40 lg:w-60 w-72" src={img} alt="Shoes" />
-            </figure>
-            <h6 className=" font-bold text-xl text-dark mt-3">{name}</h6>
-            <p className="text-center">
-                {" "}
-                <span className="text-secondary  text-lg font-bold">{price}$</span>
-            </p>
-            <p className="text-center">
-                {" "}
-                <span className="text-secondary  text-lg font-bold">Available: {quantity}</span>
-            </p>
-            <div className="card-actions justify-center">
-                <Link to='/dashboard/editproduct' state={{ _id, name, price, quantity, img }}><button className="btn btn-secondary btn-sm  rounded px-12 mt-3 ">
-                    Edit
-                </button></Link>
-                <button onClick={() => handleDelete(_id)} className="btn btn-error btn-sm  rounded px-12 mt-3 ">
-                    Delete
-                </button>
-            </div>
-        </div>
-    );
+  const handleDelete = (id) => {
+    dispatch(deleteProduct(id));
+  };
+  return (
+    <>
+      {" "}
+      <tr key={pd._id}>
+        {" "}
+        <th></th>
+        <td>{pd?.name}</td>
+        <td>${pd?.price}</td>
+        <td>{pd?.quantity}</td>
+        <td>
+          {" "}
+          <Link
+            to="/dashboard/editproduct"
+            state={{ _id, name, price, quantity, img }}
+          >
+            <MdModeEditOutline className="text-2xl text-green-600"></MdModeEditOutline>
+          </Link>
+        </td>
+        <td>
+          {" "}
+          <MdDelete
+            onClick={() => handleDelete(pd?._id)}
+            className="text-2xl text-red-500"
+          ></MdDelete>
+        </td>
+      </tr>
+    </>
+  );
 };
 
 export default AllProduct;
